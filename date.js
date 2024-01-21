0a8dd9c2b3567b07cac9fbbebc1b987d92c6dfc6
Tanha <nusrattanha897@gmail.com>
2024-01-22 01:33:43 +0600

    Date object methods

---
+++ date.js
@@ -0,0 +1,22 @@
+let myDate = new Date();
+// console.log(myDate); // Output: 2024-01-21T19:20:47.684Z
+
+// topic: toString and toLocalString
+// console.log(myDate.toString()); // Output: Mon Jan 22 2024 01:22:40 GMT+0600 (Bangladesh Standard Time)
+// console.log(myDate.toLocaleString()); // Output: 1/22/2024, 1:25:30 AM
+
+//topic: toDateString and toLocalDateString
+// console.log(myDate.toDateString()); // Output: Mon Jan 22 2024
+// console.log(myDate.toLocaleDateString()); //Output: 1/22/2024
+
+// topic: toTimeString and toLocalTimeString 
+// console.log(myDate.toTimeString()); // Output: 01:29:56 GMT+0600 (Bangladesh Standard Time)
+// console.log(myDate.toLocaleTimeString()); // Output: 1:29:56 AM
+
+//topic: toISOString
+// console.log(myDate.toISOString()); // Output: 2024-01-21T19:23:43.438Z
+
+// topic: toJSON
+// console.log(myDate.toJSON()); // Output: 2024-01-21T19:28:46.573Z
+
+
